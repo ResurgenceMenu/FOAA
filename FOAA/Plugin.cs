899f6e64fb47cb27cd8c9b20195e20fbd41609ae
Industry <lopiervright@gmail.com>
2026-04-01 15:49:53 -0700

    Add initial files

---
+++ FOAA/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace FOAA
+{
+    [BepInPlugin("industry.foaa", "Fuck Off Another Axiom", "1.0.0")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(OnGameInit);
+        }
+
+        public void OnGameInit()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
